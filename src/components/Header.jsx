3a8e315c90e55965eabbbf62c7daf9111f82b124
Fatima Zahra ELkihel <fatimazehraelkihel@gmail.com>
2024-12-25 23:37:59 +0100

    Ajout de la fonctionnalité Students

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ function Header() {
   return (
     <header style={{ textAlign: "center", margin: "20px 0" }}>
       <img
-        src="/src/assets/logo1.png"
+        src="/src/assets/logo.png"
         alt="Logo Formation"
         style={{ width: "150px", height: "auto", marginBottom: "20px" }}
       />
-      <h1>Introduction à React</h1>
+      <h1>Compte Universitaire</h1>
       <h2>A la découverte des premières notions de React</h2>
     </header>
   );
